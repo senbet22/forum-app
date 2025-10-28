b72e493281c11ef51f1d52701562e74eaf365e69
senbet22 <senbet22@users.noreply.github.com>
2025-10-28 19:08:09 +0100

    add  basic navbar styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,22 @@
-import { Link } from "@digdir/designsystemet-react";
+import { Button, Link } from "@digdir/designsystemet-react";
 
 const Navbar = () => {
   return (
-    <nav>
-      <Link href="/">Home</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/auth">Auth</Link>
-    </nav>
+    <header>
+      <nav className="flex justify-between px-8 items-center bg-amber-100 h-20">
+        <Link data-color="neutral" href="/">
+          <span className="text-black">Home</span>
+        </Link>
+        <Link href="/profile">
+          <span className="text-black">Profile</span>
+        </Link>
+        <Button asChild>
+          <Link href="/auth">
+            <span className="text-white">Login</span>
+          </Link>
+        </Button>
+      </nav>
+    </header>
   );
 };
 
